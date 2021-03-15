e1ffe8742fc13d06089e86e2c54f90e8db9bea1f
NEO0219 <neopark0219@gmail.com>
2021-03-15 16:52:44 +0900

    완료

--- DdayChat/App.js
+++ DdayChat/App.js
@@ -1,37 +1,153 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, TextInput } from 'react-native';
-
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, TextInput, ImageBackground } from 'react-native';
+import Setting from './Setting.js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dday: new Date(),
+            ddayTitle: '테스트 디데이',
+            chatLog: [],
+            chatInput: '',
+            settingModal: false,
+        }
+    }
+
+    async UNSAFE_componentWillMount() {
+        try {
+            const ddayString = await AsyncStorage.getItem('@dday');
+            const chatLogString = await AsyncStorage.getItem('@chat');
+
+            if(chatLogString == null) {
+                this.setState({chatLog: []});
+            }
+            else {
+                const chatLog = JSON.parse(chatLogString);
+                this.setState({chatLog: chatLog});
+            }
+
+            if(ddayString == null) {
+                this.setState({
+                    dday: new Date(),
+                    ddayTitle: '',
+                });
+            }
+            else {
+                const dday = JSON.parse(ddayString);
+                this.setState({
+                    dday: new Date(dday.date),
+                    ddayTitle: dday.title,
+                })
+            }
+        }
+        catch(e) {
+            console.log("ERR");
+        }
+    }
+    
+    toggleSettingModal() {
+        this.setState({
+            settingModal: !this.state.settingModal
+        })
+    }
+
+    async settingHandler(title, date) {
+        this.setState({
+            ddayTitle: title,
+            dday: date,
+        });
+        
+        try {
+            const dday = {
+                title: title,
+                date: date,
+            };
+            const ddayString = JSON.stringify(dday);
+            await AsyncStorage.setItem('@dday', ddayString);
+        }
+        catch(e) {
+            console.log(e);
+        }
+        this.toggleSettingModal();
+    }
+
+    makeDateString() {
+        return this.state.dday.getFullYear() + '년' + (this.state.dday.getMonth() + 1) + '월' + this.state.dday.getDate() + '일';
+    }
+
+    makeRemainString() {
+        const distance = new Date().getTime() - this.state.dday.getTime();
+        const remain = Math.floor(distance / (1000 * 60 * 60 * 24));
+        if(remain < 0) {
+            return 'D' + remain;
+        }
+        else if(remain > 0) {
+            return 'D+'+remain;
+        }
+        else if(remain === 0) {
+            return 'D-day';
+        }
+    }
+
+    chatHandler() {
+        this.setState({
+            chatLog: [...this.state.chatLog, this.makeDateString() + ':' + this.state.chatInput],
+            chatInput: '',
+        }), async () => {
+            const chatLogString = JSON.stringify(this.state.chatLog);
+            await AsyncStorage.setItem('@chat', chatLogString);
+        }
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <View style={styles.settingView}>
-                    <TouchableOpacity>
-                        <Image source={require('./icon/setting.png')}/>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.ddayView}>
-                    <Text>
-                        마감까지
-                    </Text>
-                    <Text>
-                        D-123
-                    </Text>
-                    <Text>
-                        2021년 11월 11일
-                    </Text>
-                </View>
-                <View style={styles.chatView}>
-                    <ScrollView>
-
-                    </ScrollView>
-                    <TextInput/>
-                    <TouchableOpacity>
-                        <Text>
-                            전송
+                <ImageBackground style={{width: '100%', height: '100%'}} source={require('./images/background.png')}>
+                    <View style={styles.settingView}>
+                        <TouchableOpacity onPress={() => this.toggleSettingModal()}>
+                            <Image source={require('./icon/setting.png')}/>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.ddayView}>
+                        <Text style={styles.titleText}>
+                            {this.state.ddayTitle}까지
                         </Text>
-                    </TouchableOpacity>
-                </View>
+                        <Text style={styles.ddayText}>
+                            {this.makeRemainString()}
+                        </Text>
+                        <Text style={styles.dateText}>
+                            {this.makeDateString()}
+                        </Text>
+                    </View>
+                    <View style={styles.chatView}>
+                        <ScrollView style={styles.chatScrollView}>                    
+                            {this.state.chatLog.map((chat)=>{
+                                return <Text style={styles.chat}>{chat}</Text>
+                            })}
+                        </ScrollView>
+                        <View style={styles.chatControl}>
+                            <TextInput 
+                                style={styles.chatInput}
+                                value={this.state.chatInput}
+                                onChangeText={(changedText) =>{
+                                    this.setState({chatInput: changedText})
+                                }}/>
+                            <TouchableOpacity 
+                                style={styles.sendButton}
+                                onPress={()=>this.chatHandler()}>
+                                <Text>
+                                    전송
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                    {this.state.settingModal ? 
+                    <Setting 
+                    modalHandler={()=>this.toggleSettingModal()}
+                    settingHandler={(title, date) => this.settingHandler(title, date)}/> : <></>}
+                </ImageBackground>
             </View>
         );
     }
@@ -42,14 +158,80 @@ const styles = StyleSheet.create({
     },
     settingView: {
         flex: 1,
-        backgroundColor: 'red',
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        marginRight: '1%',
     },
     ddayView: {
         flex: 6,
-        backgroundColor: 'green',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     chatView: {
         flex: 6,
-        backgroundColor: 'blue',
+    },
+    titleText: {
+        alignSelf: 'flex-end',
+        fontSize: 36,
+        fontWeight: 'bold',
+        color: '#4A5A5A',
+        marginRight: '15%',
+    },
+    ddayText: {
+        fontSize: 100,
+        fontWeight: 'bold',
+        color: '#4A4A4A',
+    },
+    dateText: {
+        alignSelf: 'flex-start',
+        fontSize: 21,
+        fontWeight: 'bold',
+        color: '#4A4A4A',
+        marginLeft: '15%',
+    },
+    sendButton: {
+        backgroundColor: 'rgb(97,99,250)',
+        height: 40,
+        width: 50,
+        borderRadius: 20,
+        padding: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 5,
+    },
+    chatInput: {
+        backgroundColor: 'white',
+        width: '75%',
+        height: 40,
+        borderWidth: 1,
+        borderColor: '#a5a5a5',
+        borderRadius: 20
+    },
+    chatControl: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 10,
+    },
+    chatScrollView: {
+        width: '90%',
+        margin: 10,
+        alignSelf: 'center',
+        backgroundColor: 'rgba(201,201,201,0.7)',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#a5a5a5'
+    },
+    chat: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#4A4A4A',
+        margin: 2,
+    },
+    settingView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        marginRight: '1%',
     },
 });
